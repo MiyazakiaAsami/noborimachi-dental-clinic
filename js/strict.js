e142c965d2fa743f1aa4d7c283e19e228d4d3746
MiyazakiaAsami <115138320+MiyazakiaAsami@users.noreply.github.com>
2022-11-12 03:46:34 +0900

    a

--- js/strict.js
+++ js/strict.js
@@ -17,3 +17,18 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+  //ハンバーガーメニュー
+function toggleNav() {
+  const body = document.body;
+  const hamburger = document.getElementById("js_hamburger");
+  const overlay = document.getElementById("js_overlay");
+  hamburger.addEventListener("click", function () {
+    body.classList.toggle("nav_open"); //クラスが含まれていれば削除、含まれていなければ追加する
+  });
+  overlay.addEventListener("click", function () {
+    body.classList.remove("nav_open"); //クラスを削除する
+  });
+}
+toggleNav();
